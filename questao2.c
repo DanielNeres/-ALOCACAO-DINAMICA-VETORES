94e84c27a94d7cce12b98aea88ae111d3c3e6cc5
DanielNeres <danielnerres@gmail.com>
2023-12-07 16:09:58 -0300

    questão 2 V.1.2

--- questao2.c
+++ questao2.c
@@ -4,20 +4,21 @@
 int main(){
 	int numeroquestoes, numeroalunos, i, j;
 	printf("digite a quantidede de questoes\n");
-	scanf("%d", numeroquestoes); 
+	scanf("%d", &numeroquestoes); 
 	char *gabarito = (char *) malloc(numeroquestoes * sizeof(char));
 	for(i = 0; numeroquestoes > i; i++){
 		scanf("%c", *(gabarito + i));
 	}
-	int **alunos = (int **) malloc(numeroalunos * sizeof(int));
+	char **alunos = (char **) malloc(numeroalunos * sizeof(char *));
 	for (i = 0; i < numeroalunos; i++){
-		alunos[i] = (int *) malloc(numeroquestoes * sizeof(int));
+		alunos[i] = (char *) malloc(numeroquestoes * sizeof(char));
 	}
 	for(i = 0; numeroalunos > i; i++){
 		printf("digite o gabarito do aluno %d\n", i);
 		for(j = 0; numeroquestoes > j; j++){
-			
+			scanf("%c", &alunos[i][j]);
 		}
 	}
+	
 	return 0;
 }
