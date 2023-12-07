bc9e55a3f8f6031c8bbcc1ac99569704277173f4
DanielNeres <danielnerres@gmail.com>
2023-12-07 16:15:24 -0300

    questão 2 V.1.3

--- questao2.c
+++ questao2.c
@@ -2,23 +2,27 @@
 #include<stdlib.h>
 
 int main(){
-	int numeroquestoes, numeroalunos, i, j;
-	printf("digite a quantidede de questoes\n");
-	scanf("%d", &numeroquestoes); 
-	char *gabarito = (char *) malloc(numeroquestoes * sizeof(char));
-	for(i = 0; numeroquestoes > i; i++){
-		scanf("%c", *(gabarito + i));
-	}
-	char **alunos = (char **) malloc(numeroalunos * sizeof(char *));
-	for (i = 0; i < numeroalunos; i++){
-		alunos[i] = (char *) malloc(numeroquestoes * sizeof(char));
-	}
-	for(i = 0; numeroalunos > i; i++){
-		printf("digite o gabarito do aluno %d\n", i);
-		for(j = 0; numeroquestoes > j; j++){
-			scanf("%c", &alunos[i][j]);
-		}
-	}
-	
-	return 0;
+    int numeroquestoes, numeroalunos, i, j;
+    printf("Digite a quantidade de questoes\n");
+    scanf("%d", &numeroquestoes); 
+    char *gabarito = (char *) malloc(numeroquestoes * sizeof(char));
+    for(i = 0; i < numeroquestoes; i++){
+        scanf(" %c", gabarito + i);
+    }
+    printf("Digite a quantidade de alunos\n");
+    scanf("%d", &numeroalunos);
+
+    char **alunos = (char **) malloc(numeroalunos * sizeof(char *));
+    for (i = 0; i < numeroalunos; i++){
+        alunos[i] = (char *) malloc(numeroquestoes * sizeof(char));
+    }
+
+    for(i = 0; i < numeroalunos; i++){
+        printf("Digite as respostas do aluno %d\n", i);
+        for(j = 0; j < numeroquestoes; j++){
+            scanf(" %c", &alunos[i][j]);
+        }
+    }
+
+    return 0;
 }
