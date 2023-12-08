0397dc26c0eb373dd0498566fe7d6750d8ec73e9
DanielNeres <danielnerres@gmail.com>
2023-12-08 19:48:30 -0300

    questão 1 V.3

--- questao1.c
+++ questao1.c
@@ -7,6 +7,15 @@ int main()
     scanf("%d", &numeroparticipantes);
 	char **genero = (char **) malloc(numeroparticipantes * sizeof(char *));
 	char **opinao = (char **) malloc(numeroparticipantes * sizeof(char *));
+    for (i = 0; i < numeroparticipantes; i++){
+        genero[i] = (char *) malloc(5 * sizeof(char));
+    }
+    for (i = 0; i < numeroparticipantes; i++){
+        opinao[i] = (char *) malloc(12 * sizeof(char));
+    }
+    for(i = 0; numeroparticipantes > i; i++){
+        scanf("%c", genero + i);
+    }
     for(i = 0; numeroparticipantes > i; i++){
         scanf("%c", genero + i);
     }
