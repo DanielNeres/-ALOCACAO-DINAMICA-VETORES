c1f9b259dcf27b786cff4163c169b7913208d9fa
DanielNeres <danielnerres@gmail.com>
2024-01-16 20:01:24 -0300

    questão 4

--- Lista Ponteiros/Questão 4.c
+++ Lista Ponteiros/Questão 4.c
@@ -4,7 +4,7 @@ programa deve implementar uma função chamada calcula_hexagono que calcula a á
 perímetro de um hexágono regular de lado l. A função deve obedecer ao seguinte protótipo:
 void calcula_hexagono(float l, float *area, float *perimetro); A área e o perímetro de um
 hexágono regular são dados, respectivamente, por:
-A = (3l^2√3)/2
+A = (3(l^2)√3)/2
 P = 6l
 Obs: Para os cálculos, utilize as funções sqrt e pow do módulo math da biblioteca 
 padrão de C. Em seguida, crie a função principal do programa e utilize a função 
@@ -31,6 +31,6 @@ int main()
 }
 
 void calcula_hexagono(float l, float *area, float *perimetro){
-	*area = ((3 * pow(2, l)) * sqrt(3))/2;
+	*area = ((3 * pow(l, 2)) * sqrt(3))/2;
 	*perimetro = 6 * l;
 }
