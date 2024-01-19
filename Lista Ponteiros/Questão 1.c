e2d426f6217ca9815b3e01b99b8d4dac04f6bb38
DanielNeres <danielnerres@gmail.com>
2024-01-19 11:18:08 -0300

    Questão 1

---
+++ Lista Ponteiros/Questão 1.c
@@ -0,0 +1,18 @@
+// Questão 1) Quais serão os valores de x, y e p ao final do trecho de código abaixo:
+int main (void){
+int x, y, *p;
+y = 0;
+p = &y;
+x = *p;
+x = 4;
+(*p)++;
+--x;
+(*p) += x;
+}
+
+/*
+Resposta:
+x = 3;
+y = 4;
+p = &y;
+*/
