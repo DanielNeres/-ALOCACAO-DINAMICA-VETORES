310e9a684b45ccaa0c8e2f393cd2fecab3f370c9
DanielNeres <danielnerres@gmail.com>
2024-01-21 23:54:05 -0300

    Questão 2

--- struct/Questão 2.c
+++ struct/Questão 2.c
@@ -2,122 +2,152 @@
 #include<stdlib.h>
 typedef struct{
 	char nome[40];
-	char cargo[20];
-	int identificador;
-	float salario;
-} Dadosfuncionario;
+	int NumeroDocumento;
+	int idade;
+} DadosPessoais;
 
-void TamanhoFuncionario();
 void menu();
-void RetornarMenu();
-void preencher(Dadosfuncionario *Funcionario, int quantidadefuncionarios);
-void imprimir(Dadosfuncionario *Funcionario, int quantidadefuncionarios);
-void mudarsalario(Dadosfuncionario *Funcionario, int quantidadefuncionarios);
+int RetornarMenu();
+void preencher(DadosPessoais *Pessoas, int quantidadePessoas);
+void imprimir(DadosPessoais *Pessoas, int quantidadePessoas);
+void mudarIdade(DadosPessoais *Pessoas, int quantidadePessoas);
+void MaioreMenorIdade(DadosPessoais *Pessoas, int quantidadePessoas);
 
 int main(){
-	TamanhoFuncionario();
+	menu();
 	return 0;
 }
 
-int maxfuncionarios;
-void TamanhoFuncionario(){
-	printf("digite a quantidade de funcionario maxima\n");
-	scanf("%i", &maxfuncionarios);
-	menu();
-}
 
 void menu(){
-	int escolhamenu, quantidadefuncionarios = 0;
-	Dadosfuncionario * Funcionario = (Dadosfuncionario*) malloc(sizeof(Dadosfuncionario)*maxfuncionarios);
-	if(Funcionario == NULL){
+	int escolhamenu, maxPessoas, quantidadePessoas = 0, Menu = 0;
+	printf("digite a quantidade de Pessoas maxima\n");
+	scanf("%i", &maxPessoas);
+	DadosPessoais * Pessoas = (DadosPessoais*) malloc(sizeof(DadosPessoais)*maxPessoass);
+	if(Pessoas == NULL){
 		exit(1);
 	}
-	printf("\tMENU\n");
-	printf("1. preencher informacoes do funcionario\n");
-	printf("2. imprimir informacoes do funcionario\n");
-	printf("3. preencher informacoes do funcionario\n");
+	system("cls");
+	do{
+	printf("MENU\n");
+	printf("1. preencher informacoes da pessoa\n");
+	printf("2. imprimir informacoes da pessoa\n");
+	printf("3. mudar salario dos funcionarios\n");
+	printf("4. imprecao do cargo com maior e menor salario\n");
 	printf("escolha o numero de uma das opicoes acima\n");
 
 	scanf("%i", &escolhamenu);
 	switch(escolhamenu){
 		case 1:
-			if(quantidadefuncionarios == maxfuncionarios){
+			if(quantidadePessoas == maxPessoas){
 				system("cls");
 				printf("quantidade maxima de funcionarios atingida\n\n");
-				menu();
+				Menu = 1;
+				break;
 			}
 			system("cls");
-			preencher(Funcionario, quantidadefuncionarios);
-			quantidadefuncionarios++;
-			RetornarMenu();
+			preencher(Pessoas, quantidadePessoas);
+			quantidadePessoas++;
+			Menu = RetornarMenu();
 			break;
 		case 2:
-			if(quantidadefuncionarios == 0){
+			if(quantidadePessoas == 0){
 				system("cls");
 				printf("nenhum funcionario cadastrado\n\n");
-				menu();
+				Menu = 1;
+				break;
 			}
 			system("cls");
-			imprimir(Funcionario, quantidadefuncionarios);
-			RetornarMenu();
+			imprimir(Pessoas, quantidadePessoas);
+			Menu = RetornarMenu();
 			break;
 		case 3:
-			if(quantidadefuncionarios == 0){
+			if(quantidadePessoas == 0){
 				system("cls");
 				printf("nenhum funcionario cadastrado\n\n");
-				menu();
+				Menu = 1;
+				break;
 			}
 			system("cls");
-			mudarsalario(Funcionario, quantidadefuncionarios);
-			RetornarMenu();
+			mudarsalario(Pessoas, quantidadePessoas);
+			Menu = RetornarMenu();
+			break;
+			case 4:
+			if(quantidadePessoas == 0){
+				system("cls");
+				printf("nenhum funcionario cadastrado\n\n");
+				Menu = 1;
+				break;
+			}
+			system("cls");
+			MaioreMenorSalario(Pessoas, quantidadePessoas);
+			Menu = RetornarMenu();
+			break;
 		default:
 			exit(1);
 			break;
-	}
+		}
+		
+	}while(Menu == 1);
 }
 
-void RetornarMenu(){
+int RetornarMenu(){
 	char retornoMenu;
 	printf("caso desejar retornar para o menu digite M, ou digite qualquer tecla para sair\n");
 	while (getchar() != '\n');
 	retornoMenu = getchar();
 	if(retornoMenu == 'M' || retornoMenu == 'm'){
 		system("cls");
-		menu();
+		return 1;
+	}else
+	{
+		system("cls");
+		return 0;
 	}
 }
 
-void preencher(Dadosfuncionario *Funcionario, int quantidadefuncionarios){
-	printf("preencha dados do funcionario %d\n", quantidadefuncionarios + 1);
+void preencher(DadosPessoais *Pessoas, int quantidadePessoas){
+	printf("preencha dados da pessoa %d\n", quantidadePessoas + 1);
 	printf("digite o nome\n");
-	scanf(" %[^\n]", Funcionario[quantidadefuncionarios].nome);
-	printf("digite a cargo\n");
-	scanf(" %[^\n]", Funcionario[quantidadefuncionarios].cargo);
-	printf("digite a identificacao\n");
-	scanf("%d", &Funcionario[quantidadefuncionarios].identificador);
-	printf("digite o salario\n");
-	scanf("%f", &Funcionario[quantidadefuncionarios].salario);
+	scanf(" %[^\n]", Pessoas[quantidadePessoas].nome);
+	printf("digite o numero do documento\n");
+	scanf("%d", &Funcionario[quantidadePessoas].NumeroDocumento);
+	printf("digite a idade\n");
+	scanf("%d", &Funcionario[quantidadePessoas].idade);
 }
 
-void mudarsalario(Dadosfuncionario *Funcionario, int quantidadefuncionarios){
+void imprimir(DadosPessoais *Pessoas, int quantidadePessoas){
+	int i;
+	for(i = 0; i < quantidadePessoas; i++){
+    	printf("dados da pessoa %d\n", i + 1);
+		printf("nome: %s\n", Pessoas[i].nome);
+		printf("numero do documento: %d\n", Pessoas[i].NumeroDocumento);
+		printf("salario: R$%d\n\n", Pessoas[i].idade);
+	}
+}
+
+void mudarIdade(DadosPessoais *Pessoas, int quantidadePessoas){
 	int escolhaMudar, i;
-	printf("escolha o funcionario ");
-	for(i = 1; i <= quantidadefuncionarios; i++){
+	printf("escolha a pessoa entre os numeros ");
+	for(i = 1; i <= quantidadePessoas; i++){
 		printf("%d ", i);
 	}
+	printf("para mudar a idade\n");
 	scanf("%d", &escolhaMudar);
-	printf("para mudar o salario\n");
-	printf("digite o novo salario do funcionario\n");
-	scanf("%f", &Funcionario[escolhaMudar - 1].salario);
+	printf("digite o nova idade da pessoa\n");
+	scanf("%d", &Funcionario[escolhaMudar - 1].idade);
 }
 
-void imprimir(Dadosfuncionario *Funcionario, int quantidadefuncionarios){
-	int i;
-	for(i = 0; i < quantidadefuncionarios; i++){
-    	printf("dados do funcionario %d\n", (i + 1));
-		printf("nome: %s\n", Funcionario[i].nome);
-		printf("cargo: %s\n", Funcionario[i].cargo);
-		printf("identificacao: %d\n", Funcionario[i].identificador);
-		printf("salario: %f\n", Funcionario[i].salario);
+void MaioreMenorIdade(DadosPessoais *Pessoas, int quantidadePessoas){
+	int i, MaiorIdade = 0, MenorIdade = 0;
+	for(i = 1; i < quantidadePessoas; i++){
+    	if(Funcionario[i].idade < Funcionario[MenorIdade].idade){
+			MenorIdade = i;
+		}
+		if(Funcionario[i].idade > Funcionario[MaiorIdade].idade){
+			MaiorIdade = i;
+		}
 	}
+	printf("%s e o cargo que recebe menos, com a idade de R$%d;\n", Pessoas[MenorIdade].cargo, Pessoas[MenorIdade].idade);
+	printf("%s e o cargo que recebe mais, com a idade de R$%d.\n", Pessoas[MaiorIdade].cargo, Pessoas[MaiorIdade].idade);
 }
