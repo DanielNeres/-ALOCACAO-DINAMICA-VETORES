c33aeb7602f575724b5f711ff4938b2c7d2c407e
DanielNeres <danielnerres@gmail.com>
2024-01-22 19:41:57 -0300

    Questão 1

--- Struct, Union e Enum/Questão 1.c
+++ Struct, Union e Enum/Questão 1.c
@@ -2,6 +2,61 @@
 #include<stdlib.h>
 #include<string.h>
 
+typedef enum
+{
+	MASCULINO = 1, FEMININO
+} genero;
+
+typedef struct
+{
+	char nome[20];
+	int idade;
+	genero var_genero;
+} pessoa;
+
+void preencher(pessoa *p)
+{
+	// funÃ§Ã£o que preenche os dados de uma variavel tipo pessoa (struct)
+	int escolha;
+	printf("Digite o nome: ");
+	scanf(" %[^\n]", p->nome);
+	printf("Digite a idade: ");
+	scanf("%d", &p->idade);
+	printf("digite 1 para MASCULINO ou 2 para FEMININO\n");
+	scanf("%d", &escolha);
+	switch(escolha)
+	{
+	case 1:
+		strcpy(p->var_genero.Masculino, "Masculino");
+		break;
+	case 2:
+		strcpy(p->var_genero.Feminino, "Feminino");
+		break;
+
+	default:
+		exit(1);
+		break;
+	}
+}
+
+void imprimir(pessoa *p){
+	printf("nome: %s\n", p->nome);
+	printf("idade: %d\n", p->idade);
+	printf("Genero: %s\n", p->var_genero.Feminino);
+}
+
+int main()
+{
+	pessoa *P = malloc(sizeof(pessoa));
+	preencher(P);
+	imprimir(P);
+	free(P);
+	return 0;
+}
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
 typedef union
 {
 	char Masculino[20];
@@ -17,7 +72,7 @@ typedef struct
 
 void preencher(pessoa *p)
 {
-	// funÃ§Ã£o que preenche os dados de uma variavel tipo pessoa (struct)
+	// função que preenche os dados de uma variável tipo pessoa (struct)
 	int escolha;
 	printf("Digite o nome: ");
 	scanf(" %[^\n]", p->nome);
