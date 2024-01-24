e82fcd86656e296f119962f34ce4a6dec3890c28
DanielNeres <danielnerres@gmail.com>
2024-01-23 22:07:04 -0300

    Questão 3

--- Atividade Enum/Questao3.c
+++ Atividade Enum/Questao3.c
@@ -6,7 +6,7 @@ typedef enum
 {
     janeiro = 1,
     fevereiro,
-    março,
+    marco,
     abril,
     maio,
     junho,
@@ -21,71 +21,13 @@ typedef enum
 typedef struct
 {
     int dia;
-    char mes[15];
+    int mes;
     int ano;
 
 } data;
 
-void preencher(data *p)
-{
-    // funÃ§Ã£o que preenche os dados de uma variavel tipo pessoa (struct)
-    int escolha;
-    printf("Digite o dia: ");
-    scanf("%d", data_var->dia);
-    printf("digite o mes:\n");
-    scanf(" [^\n]", data_var->mes);
-    printf("digite o ano:\n");
-    scanf("%d", data_var->mes);
-}
-
-void imprimir(data *p)
-{
-    printf("dia: %s\n", data_var->dia);
-    printf("mes: %f\n", data_var->mes);
-    printf("tipo: ");
-    switch (escolha)
-    {
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    case janeiro:
-        printf("01\n");
-        break;
-    default:
-        exit(1);
-        break;
-    }
-}
+void preencher(data *data_var);
+void imprimir(data *data_var);
 
 int main()
 {
@@ -95,3 +37,64 @@ int main()
     free(data_var);
     return 0;
 }
+
+void preencher(data *data_var)
+{
+    int escolha;
+    printf("Digite o dia: ");
+    scanf("%d", &data_var->dia);
+    printf("digite o mes:\n");
+    scanf("%d", data_var->mes);
+    printf("digite o ano:\n");
+    scanf("%d", &data_var->mes);
+}
+
+void imprimir(data *data_var)
+{
+    printf("dia: %02d\n", data_var->dia);
+    printf("mes: %02d\n", data_var->mes);
+    switch (data_var->dia)
+    {
+    case janeiro:
+        printf("01\n");
+        break;
+    case fevereiro:
+        printf("02\n");
+        break;
+    case marco:
+        printf("03\n");
+        break;
+    case abril:
+        printf("04\n");
+        break;
+    case maio:
+        printf("05\n");
+        break;
+    case junho:
+        printf("06\n");
+        break;
+    case julho:
+        printf("07\n");
+        break;
+    case agosto:
+        printf("08\n");
+        break;
+    case setembro:
+        printf("09\n");
+        break;
+    case outubro:
+        printf("10\n");
+        break;
+    case novembro:
+        printf("11\n");
+        break;
+    case dezembro:
+        printf("12\n");
+        break;
+    default:
+        exit(1);
+        break;
+    }
+     printf("ano: %04d", data_var->dia);
+}
+
