7f309eebf0fa2716e423941954514e8da70e6e96
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2024-01-28 13:34:22 -0300

    Questão 1

--- Atividade Union/Questao1.c
+++ Atividade Union/Questao1.c
@@ -1,3 +1,6 @@
+/*1. Escreva um programa que implementa uma struct Pessoa com os campos nome, idade e gênero. O
+gênero deve ser armazenado como um enum com os valores MASCULINO e FEMININO. O programa
+deve ler os dados de uma pessoa e imprimir os dados da pessoa.*/
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
