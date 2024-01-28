68dee0034bbb8c26586c72d698c27912d38e23b8
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2024-01-28 13:30:33 -0300

    Update Questao2.c

--- Atividade Union/Questao2.c
+++ Atividade Union/Questao2.c
@@ -1,5 +1,5 @@
-/*1. Escreva um programa que implementa uma struct Pessoa com os campos nome, idade e gˆenero. O
-gˆenero deve ser armazenado como um enum com os valores MASCULINO e FEMININO. O programa
+/*1. Escreva um programa que implementa uma struct Pessoa com os campos nome, idade e gênero. O
+gênero deve ser armazenado como um enum com os valores MASCULINO e FEMININO. O programa
 deve ler os dados de uma pessoa e imprimir os dados da pessoa.*/
 
 #include<stdio.h>
