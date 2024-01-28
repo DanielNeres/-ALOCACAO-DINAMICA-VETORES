cea1cf915f1bcf50e46ae5bcd644c5c03cba0c02
DanielNeres <danielnerres@gmail.com>
2024-01-28 12:45:12 -0300

    Questão 1

--- struct/Questão 1.c
+++ struct/Questão 1.c
@@ -1,3 +1,14 @@
+/*1. Crie um tipo estruturado para armazenar dados de um funcionário. Uma estrutura deste tipo possui
+os seguintes campos: nome, salário, identificador e cargo.
+a) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Funcionario
+e preencha seus campos com valores fornecidos pelo usuário via teclado.
+b) Escreva uma função que receba como parâmetro o endereço de uma estrutura do tipo Funcionario
+e imprima os valores dos seus campos.
+c) Implemente uma função para realizar uma alteração no salário de uma estrutura do tipo Funcionario.
+d) Escreva uma função que receba como parâmetro um vetor de estruturas do tipo Funcionario e
+imprima o cargo e salário do Funcionario com maior salário e o cargo e salário do funcionário
+com o menor salário.*/
+
 #include<stdio.h>
 #include<stdlib.h>
 typedef struct{
@@ -155,9 +166,3 @@ void MaioreMenorSalario(Dadosfuncionario *Funcionario, int quantidadefuncionario
 	printf("%s e o cargo que recebe menos, com o salario de R$%f;\n", Funcionario[MenorSalario].cargo, Funcionario[MenorSalario].salario);
 	printf("%s e o cargo que recebe mais, com o salario de R$%f.\n", Funcionario[MaiorSalario].cargo, Funcionario[MaiorSalario].salario);
 }
-
-
-
-
-
-
