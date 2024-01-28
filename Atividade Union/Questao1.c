ebae47f0c021e8c0ce8d3706ae8b024a17f1a5cb
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2024-01-28 14:02:02 -0300

    Questão 1

--- Atividade Union/Questao1.c
+++ Atividade Union/Questao1.c
@@ -1,62 +1,7 @@
 /*1. Escreva um programa que implementa uma struct Pessoa com os campos nome, idade e gênero. O
 gênero deve ser armazenado como um enum com os valores MASCULINO e FEMININO. O programa
 deve ler os dados de uma pessoa e imprimir os dados da pessoa.*/
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
 
-typedef enum
-{
-	MASCULINO = 1, FEMININO
-} genero;
-
-typedef struct
-{
-	char nome[20];
-	int idade;
-	genero var_genero;
-} pessoa;
-
-int preencher(pessoa *p)
-{
-	// funÃ§Ã£o que preenche os dados de uma variavel tipo pessoa (struct)
-	int escolha;
-	printf("Digite o nome: ");
-	scanf(" %[^\n]", p->nome);
-	printf("Digite a idade: ");
-	scanf("%d", &p->idade);
-	printf("digite 1 para MASCULINO ou 2 para FEMININO\n");
-	scanf("%d", &escolha);
-	switch(escolha)
-	{
-	case 1:
-		return escolha;
-		break;
-	case 2:
-		return escolha;
-		break;
-	default:
-		exit(1);
-		break;
-	}
-}
-
-void imprimir(pessoa *p, int escolha){
-	printf("nome: %s\n", p->nome);
-	printf("idade: %d\n", p->idade);
-	printf("Genero: %s\n", p->var_genero.escolha);
-}
-
-int main()
-{
-	int escolha;
-	escolha = preencher();
-	pessoa *P = malloc(sizeof(pessoa));
-	preencher(P);
-	imprimir(P, escolha);
-	free(P);
-	return 0;
-}
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
