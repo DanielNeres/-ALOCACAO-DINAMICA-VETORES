3e2961612771e28162d4b432bbd6efd7b3ccedcf
DanielNeres <danielnerres@gmail.com>
2024-02-02 10:08:32 -0300

    Questão 2

--- Unidade-II/questoes-pratica-I/questao2.c
+++ Unidade-II/questoes-pratica-I/questao2.c
@@ -1,3 +1,20 @@
+/*Questão 2) Escreva um programa em C que preencha um vetor de 10 inteiros com informações
+vindas de um arquivo e escreva em outro arquivo o menor elemento, o maior elemento, bem
+como a média dos elementos do vetor, como ilustrado a seguir.
+
+Exemplo oficial:
+10
+2
+8
+1
+12
+8
+10
+5
+4
+19
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -10,7 +27,14 @@ FILE *arquivo = fopen("Entrada_q2.txt", "wt");
         exit(1);
     }
     int tamanhovetor;
-    printf("digite o tamanho do vetor");
+    printf("digite o tamanho do vetor\n");
     scanf("%d", &tamanhovetor);
-    int *vetor[10] = malloc(sizeof(int) * tamanhovetor);
+    int *vetor = malloc(sizeof(int) * tamanhovetor);
+    for(;tamanhovetor > 0;tamanhovetor--){
+    printf("digite um numero");
+    scanf("%d", vetor[tamanhovetor]);
+    fprintf(arquivo, "%d", vetor[tamanhovetor]);
+    }
+    fclose(arquivo);
+    return 0;
 }
