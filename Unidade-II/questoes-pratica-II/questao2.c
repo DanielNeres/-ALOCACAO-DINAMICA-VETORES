f19194d48017ee800bddccff29c569758fe7474c
DanielNeres <danielnerres@gmail.com>
2024-02-03 10:50:57 -0300

    Questão 2

--- Unidade-II/questoes-pratica-II/questao2.c
+++ Unidade-II/questoes-pratica-II/questao2.c
@@ -9,18 +9,19 @@ int main()
         printf("Erro!!!");
         return 1;
     }
-    int QuantidadeNotas, QuantidadeAlunos, i;
+    int QuantidadeNotas, QuantidadeAlunos, i, j;
     printf("digite quantas notas os alunos tem alunos\n");
     float *notas = malloc(sizeof(float)*QuantidadeNotas);
     printf("digite a quantidade de alunos\n");
     scanf("%d", &QuantidadeAlunos);
     for (i = 1; QuantidadeAlunos >= i; i++)
     {
-        for (j = 1; j <= 3; j++)
+        fprintf(arquivo, "notas do aluno %d\n", i);
+        for (j = 0; j <= QuantidadeNotas; j++)
         {
-            printf("digite a nota %d: \t", j);
-            scanf("%f", &nota);
-            fprintf(arquivo, "\t%.2f", nota);
+            printf("digite a nota %d: \t", j + 1);
+            scanf("%f", &notas[j]);
+            fprintf(arquivo, "\t%.2f", notas[j]);
         }
         fprintf(arquivo, "\n\n");
     }
