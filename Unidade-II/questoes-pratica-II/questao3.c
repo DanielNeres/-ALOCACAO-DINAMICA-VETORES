dbd3080dced6cfc4c46b120b1533ecb0b54c351b
DanielNeres <danielnerres@gmail.com>
2024-02-03 13:26:27 -0300

    Questão 3

--- Unidade-II/questoes-pratica-II/questao3.c
+++ Unidade-II/questoes-pratica-II/questao3.c
@@ -1,23 +1,50 @@
+/*Você foi contratado para desenvolver um programa em C que permita 
+o cadastro de frutas e seus preços em um arquivo de dados. O programa
+deve usar uma estrutura (struct) para armazenar os detalhes de cada fruta, 
+incluindo o nome da fruta e o preço.
+
+O programa deve realizar as seguintes ações:
+
+Definir uma estrutura chamada Fruta com os seguintes campos:
+
+nome (string) para armazenar o nome da fruta.
+preço (float) para armazenar o preço da fruta.
+
+Permitir que o usuário insira os dados das frutas via teclado. 
+O usuário poderá cadastrar múltiplas frutas em uma única execução 
+do programa. Para cada fruta, o programa deve solicitar:
+
+O nome da fruta.
+O preço da fruta.
+
+Após inserir os dados de cada fruta, o programa deve salvar as 
+informações no arquivo ‘frutas.txt’. Cada linha do arquivo deve 
+conter o nome da fruta e seu preço, separados por vírgula.
+
+O programa deve continuar solicitando os dados das frutas até que 
+o usuário decida parar. Quando o usuário decidir parar de cadastrar 
+frutas, o programa deve exibir uma mensagem de encerramento e fechar o arquivo.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 typedef struct
 {
     char Nome[40];
-    float Preco;
+    float preco;
 }Frutas;
  
 
 int main()
 {
-    FILE *arquivo = fopen("Frutas.txt", "wt");
+    FILE *arquivo = fopen("frutas.txt", "wt");
     if (arquivo == NULL)
     {
         printf("Erro!!!");
         return 1;
     }
     Frutas frutas_var;
-    int permanencia = 1, i;
+    int permanencia = 1, i = 1;
     char escolhapermanencia;
     do
     {
@@ -28,16 +55,18 @@ int main()
         scanf(" %s", frutas_var.Nome);
         fprintf(arquivo, "Nome: %s\t", frutas_var.Nome);
 
-        printf("digite o ID do funcionario %d:\n", i);
-        scanf("%d", &frutas_var.Preco);
-        fprintf(arquivo, "ID: %d\n\n", frutas_var.Preco);
+        printf("digite o preco %d:\n", i);
+        scanf("%d", &frutas_var.preco);
+        fprintf(arquivo, "ID: %d\n\n", frutas_var.preco);
 
-        printf("Se deseja continuar fazendo cadastro digite 'C'");
-        scanf("%c", &escolhapermanencia);
+        printf("Se deseja continuar fazendo cadastro digite 'C'\n");
+        scanf(" %c", &escolhapermanencia);
         if(escolhapermanencia != 'c' || escolhapermanencia != 'C'){
             permanencia = 1;
+            printf("Programa encerrado com sucesso");
+            return 0;
         }
-
+        i++;
     }while (permanencia != 0);
     
     fclose(arquivo);
