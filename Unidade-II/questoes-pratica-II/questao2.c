a21dbbeaec0f68acee313ff7c3b814b738f7ef18
DanielNeres <danielnerres@gmail.com>
2024-02-03 10:43:51 -0300

    Questão 2

--- Unidade-II/questoes-pratica-II/questao2.c
+++ Unidade-II/questoes-pratica-II/questao2.c
@@ -3,30 +3,26 @@
 
 int main()
 {
-    FILE *arquivo = fopen("Entrada_q1.txt", "wt");
+    FILE *arquivo = fopen("NotasAlunos.txt", "wt");
     if (arquivo == NULL)
     {
         printf("Erro!!!");
         return 1;
     }
-    char nome[40];
-    int QuantidadeAlunos, i, j;
-    float nota;
-    float notas[3];
+    int QuantidadeNotas, QuantidadeAlunos, i;
+    printf("digite quantas notas os alunos tem alunos\n");
+    float *notas = malloc(sizeof(float)*QuantidadeNotas);
     printf("digite a quantidade de alunos\n");
     scanf("%d", &QuantidadeAlunos);
     for (i = 1; QuantidadeAlunos >= i; i++)
     {
-        printf("digite o nome do aluno %d:\t", i);
-        scanf(" %s", nome);
-        fprintf(arquivo, "%s\t", nome);
         for (j = 1; j <= 3; j++)
         {
             printf("digite a nota %d: \t", j);
             scanf("%f", &nota);
             fprintf(arquivo, "\t%.2f", nota);
         }
-        fprintf(arquivo, "\n");
+        fprintf(arquivo, "\n\n");
     }
     fclose(arquivo);
 }
