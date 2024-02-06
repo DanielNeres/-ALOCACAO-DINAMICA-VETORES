b116504f0478bed76fe9fff5b85e8d694f3f19cf
DanielNeres <danielnerres@gmail.com>
2024-02-06 10:39:37 -0300

    Adição operações.h

---
+++ Unidade-II/modulo/Operacoes/operacoes.h
@@ -0,0 +1,21 @@
+
+
+float AoQuadrado(float numero);
+
+
+float AoCubo(float numero);
+
+
+float Potencia(float numero, int expoente);
+
+
+float Soma(float a, float b);
+
+
+float Subtracao(float a, float b);
+
+
+float Multiplicacao(float a, float b);
+
+
+float divisao(float a, float b);
