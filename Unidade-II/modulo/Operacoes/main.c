c896c06a54337267fba7c40c736b8a2c07c9579d
DanielNeres <danielnerres@gmail.com>
2024-02-06 10:39:49 -0300

    Adição main.c

---
+++ Unidade-II/modulo/Operacoes/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "tentandofazer.h"
+
+int main(){
+    float numeroa, numerob, expoente;
+    printf("digite\n");
+    scanf("%f", &numeroa);
+    printf("digite\n");
+    scanf("%f", &numerob);
+    float soma = Soma(numeroa, numerob);
+    float subtracao = Subtracao(numeroa, numerob);
+    float multiplicacao = Multiplicacao(numeroa, numerob);
+    float Divisao = divisao(numeroa, numerob);
+    printf("%f\n", soma);
+    printf("%f\n", subtracao);
+    printf("%f\n", multiplicacao);
+    printf("%f\n", Divisao);
+
+}
