625ccf8c7e1d672f6984858775fd470178d2edcf
DanielNeres <danielnerres@gmail.com>
2024-02-07 20:11:43 -0300

    Criação struct funcionario

---
+++ Unidade-II/TAD/Questao2/TAD2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "TAD2.h"
+
+struct funcionario{
+    char Funcional[3];
+    char Nome[60];
+    char Departamento;
+    float Salario;
+};
+
+void copia_dados(FILE * fl, int n, Funcionario **pessoal){
+
+
+}
