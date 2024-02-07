7c8146b902055c723f38695ea0ce8b88248b1503
DanielNeres <danielnerres@gmail.com>
2024-02-07 20:23:21 -0300

    Criação da função copia_dados

--- Unidade-II/TAD/Questao2/TAD2.c
+++ Unidade-II/TAD/Questao2/TAD2.c
@@ -1,14 +1,31 @@
 #include <stdio.h>
 #include "TAD2.h"
 
-struct funcionario{
+struct funcionario
+{
     char Funcional[3];
     char Nome[60];
     char Departamento;
     float Salario;
 };
 
-void copia_dados(FILE * fl, int n, Funcionario **pessoal){
+void copia_dados(FILE *fl, int n, Funcionario **pessoal)
+{
 
+    int i;
+    for (i = 0; i < n; i++)
+    {
+        printf("Digite os dados do Funcionario %d\n\n", i + 1);
+        printf("Digite o funcional do Funcionario: \t");
+        scanf(" %[^\n]", pessoal[i]->Funcional);
 
+        printf("Digite o nome do Funcionario: \t");
+        scanf(" %[^\n]", pessoal[i]->Nome);
+
+        printf("Digite o Departamento do Funcionario: \t");
+        scanf(" %c", pessoal[i]->Departamento);
+
+        printf("Digite o salario do Funcionario: \t");
+        scanf(" %[^\n]", pessoal[i]->Salario);
+    }
 }
