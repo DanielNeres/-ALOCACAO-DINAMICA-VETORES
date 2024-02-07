9d357f63a9a04adca74ccfcb0e242ffcf14fa986
DanielNeres <danielnerres@gmail.com>
2024-02-07 16:20:15 -0300

    Criação da Função preencher

--- Unidade-II/TAD/Questao1/TAD1.c
+++ Unidade-II/TAD/Questao1/TAD1.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "TAD1.h"
+
 typedef struct
 {
 	float preco;
 	char local[60];
 	char atracao[60];
 } Ingresso;
+
+void preencher(Ingresso * i, int QuantidadeIngresso)
+	{
+		printf("preencha dados do ingresso %d\n", QuantidadeIngresso + 1);
+		printf("digite o local\n");
+		scanf(" %[^\n]", i[QuantidadeIngresso].local);
+		printf("digite a atracao\n");
+		scanf(" %[^\n]", i[QuantidadeIngresso].atracao);
+		printf("digite o preco\n");
+		scanf("%f", &i[QuantidadeIngresso].preco);
+	}
