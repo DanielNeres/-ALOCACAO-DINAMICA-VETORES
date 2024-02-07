ce71e6b4d5379b1a2a532bb26cff0594470a6a63
DanielNeres <danielnerres@gmail.com>
2024-02-07 20:31:47 -0300

    Declaração da função imprime_folha_pagamento

--- Unidade-II/TAD/Questao2/TAD2.c
+++ Unidade-II/TAD/Questao2/TAD2.c
@@ -15,6 +15,7 @@ void copia_dados(FILE *fl, int n, Funcionario **pessoal)
     int i;
     for (i = 0; i < n; i++)
     {
+        fopen(fl, "wr");
         printf("Digite os dados do Funcionario %d\n\n", i + 1);
         printf("Digite o funcional do Funcionario: \t");
         scanf(" %[^\n]", pessoal[i]->Funcional);
@@ -28,4 +29,8 @@ void copia_dados(FILE *fl, int n, Funcionario **pessoal)
         printf("Digite o salario do Funcionario: \t");
         scanf(" %[^\n]", pessoal[i]->Salario);
     }
+}
+
+void imprime_folha_pagamento(int n, Funcionario ** pessoal, char depto){
+
 }
