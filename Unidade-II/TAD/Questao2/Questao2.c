1d01804ed007335f6ef274077c5ac089677ab8de
DanielNeres <danielnerres@gmail.com>
2024-02-08 12:38:05 -0300

    chamada da função

--- Unidade-II/TAD/Questao2/Questao2.c
+++ Unidade-II/TAD/Questao2/Questao2.c
@@ -5,11 +5,15 @@
 int main(){
 
     int n;
+    char depto;
     printf("Digite a quantidade de funcionarios");
     scanf("%d", &n);
     Funcionario **pessoal = (Funcionario **)malloc(sizeof(Funcionario*) * n);
     FILE *fl = fopen("Funcionario.txt", "wr");
     void copia_dados(FILE *fl, int n, Funcionario **pessoal);
+    printf("Digite o departento");
+    scanf("%c", &depto);
+    void imprime_folha_pagamento(n, pessoal, depto);
 
     return 0;
 }
