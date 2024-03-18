c43a0efa1cea29f6b567c2557776c1dbb037ba72
DanielNeres <danielnerres@gmail.com>
2024-03-18 16:10:00 -0300

    pilha

--- Unidade-III/Pilha/pratica_pilha/pilha.c
+++ Unidade-III/Pilha/pratica_pilha/pilha.c
@@ -1,39 +1,38 @@
 #include "pilha.h"
 
-struct pilha
-{
-    float valor;
-    Pilha *proxima;
-};
-
 Pilha* pilha_cria(void){
 
     Pilha *pilha_var = NULL;
     return pilha_var;
 }
 
-void pilha_push(Pilha* pilha, float valor){
-    
+void pilha_push(Pilha **pilha, float valor) {
+
     Pilha *pilha_var = (Pilha *)malloc(sizeof(Pilha));
-    if (pilha_var == NULL){
+    if (pilha_var == NULL) {
         printf("Erro de alocacao\n");
+        return;
     }
     pilha_var->valor = valor;
-    pilha->proxima = pilha_var;
-
+    pilha_var->proxima = *pilha;
+    *pilha = pilha_var;
 }
 
 
-float pilha_pop(Pilha* pilha){
+float pilha_pop(Pilha **pilha) {
 
-    Pilha *key = pilha;
+    if (*pilha == NULL) {
+        printf("Pilha vazia\n");
+        return -1;
+    }
+    Pilha *key = *pilha;
     float valor = key->valor;
-    pilha = pilha->proxima;
+    *pilha = (*pilha)->proxima;
     free(key);
     return valor;
 }
 
-int pilha_vazia(Pilha* pilha){
+int pilha_vazia(Pilha *pilha){
 
     int pilha_vazia;
     if (pilha == NULL){
@@ -42,13 +41,11 @@ int pilha_vazia(Pilha* pilha){
     return 0;
 }
 
-void pilha_libera(Pilha* pilha){
-
-    while (pilha == NULL)
-    {
-        Pilha *key = pilha;
-        pilha = pilha->proxima;
+void pilha_libera(Pilha **pilha) {
+    
+    while (*pilha != NULL) {
+        Pilha *key = *pilha;
+        *pilha = (*pilha)->proxima;
         free(key);
     }
-    
 }
--- Unidade-III/Pilha/pratica_pilha/pilha.h
+++ Unidade-III/Pilha/pratica_pilha/pilha.h
@@ -1,10 +1,19 @@
+#ifndef PILHA_H
+#define PILHA_H
+
+
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct pilha Pilha;
+typedef struct Pilha {
+    float valor;
+    struct pilha *proxima;
+} Pilha;
 
 Pilha* pilha_cria(void);
-void pilha_push(Pilha* ppilha, float valor);
-float pilha_pop(Pilha* ppilha);
-int pilha_vazia(Pilha* pilha);
-void pilha_libera(Pilha* pilha);
+void pilha_push(Pilha **pilha, float valor);
+float pilha_pop(Pilha *pilha);
+int pilha_vazia(Pilha *pilha);
+void pilha_libera(Pilha *pilha);
+
+#endif // PILHA_H
