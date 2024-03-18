5567109519e6c1e6c9e1e09f39f70563349daf52
DanielNeres <danielnerres@gmail.com>
2024-03-18 16:09:49 -0300

    pilha

--- Unidade-III/Pilha/pratica_pilha/main.c
+++ Unidade-III/Pilha/pratica_pilha/main.c
@@ -2,15 +2,20 @@
 
 int main(){
 
+    printf("estou aqui1\n");
     Pilha *pilha_var = pilha_cria();
     pilha_push(pilha_var, 4);
     pilha_push(pilha_var, 9);
+    printf("%d\n", pilha_var->valor);
+    printf("estou aqui2");
     float valor = pilha_pop(pilha_var);
-    printf("%d\n", valor);
+    printf("%f\n", valor);
     int vazia = pilha_vazia(pilha_var);
+    printf("estou aqui3");
     printf("%d\n", vazia);
     pilha_libera(pilha_var);
-    int vazia = pilha_vazia(pilha_var);
+    vazia = pilha_vazia(pilha_var);
     printf("%d\n", vazia);
+    printf("estou aqui4");
 
 }
